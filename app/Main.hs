7da0b8c2a56d958b22f1d7185e4101b1d729d838
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-12-02 21:06:14 +0100

    main

---
+++ app/Main.hs
@@ -0,0 +1,18 @@
+-- |
+-- Module      :  Main
+-- Description :  Minesweeper
+-- Copyright   :  2022 Dominik Schrempf
+-- License     :  GPL-3.0-or-later
+--
+-- Maintainer  :  dominik.schrempf@gmail.com
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Creation date: Fri Dec  2 21:06:01 2022.
+module Main
+  ( main,
+  )
+where
+
+main :: IO ()
+main = undefined
